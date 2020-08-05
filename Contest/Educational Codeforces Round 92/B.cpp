ebc25582a871ecc850fa184cad9cdb81dc37134e
CrazyDave <dave@CrazyDavedeMacBook-Pro.local>
2020-08-05 23:12:56 +0800

    qwq

---
+++ Contest/Educational Codeforces Round 92/B.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+
+    printf("FUCK\n"); 
+    return 0;
+}
