a4e9be4f7da46a8ab9b509b1cf4ece2ec9acbb77
CrazyDave <dave@CrazyDavedeMacBook-Pro.local>
2020-08-23 16:21:11 +0800

    restart

--- restart/CF1389D.cpp
+++ restart/CF1389D.cpp
@@ -8,30 +8,35 @@
 #define MAXN 
 using namespace std;
 
-int t, n, k, l1, r1, l2, r2, len1, len2;
+LL t, n, k, l1, r1, l2, r2;
 
 int main()
 {
-    scanf("%d", &t);
+    scanf("%lld", &t);
     for(rint i=1; i<=t; ++i)
     {
         LL ans=0, temp=0;
-        scanf("%d%d%d%d%d%d", &n, &k, &l1, &r1, &l2, &r2);
-        len1=r1-l1, len2=r2-l2;
+        scanf("%lld%lld%lld%lld%lld%lld", &n, &k, &l1, &r1, &l2, &r2);
+        LL len1=r1-l1, len2=r2-l2;
+        if(l1==r1 && l2==r2 && l1==l2)
+        {
+            printf("%lld\n", k*2);
+            continue;
+        }
         if(r1>l2 && l1<r2)
         {
             if((r1>=r2 && l1<=l2) || (r1<=r2 && l1>=l2)) temp=min(r1-l1, r2-l2);
             else temp=min(r1-l2, r2-l1);
             if(temp*n>=k) ans=0;
             else if((len1+len2-temp)*n>=k) ans=k-temp*n;
-            else ans=(len1+len2-temp*2)*n+2*(k-(len1+len2-temp)*n);
+            else ans=(len1+len2-temp*2)*n+(k-(len1+len2-temp)*n)*2;
         }
         else
         {
-            if(l2>=r1) temp=l2-r1;
-            else temp=l1-r2;
+            temp=max(l2-r1, l1-r2);
             LL cost=temp*2+len1+len2, val=temp+len1+len2, num=k/val;
-            if(num>n) ans=2*(k-n*val)+n*cost;
+            if(num>=n) ans=(k-n*val)*2+n*cost;
+            else if(num==0) ans=temp+(k-num*val);
             else ans=cost*num+min((k-num*val)*2, temp+(k-num*val));
         }
         printf("%lld\n", ans);
