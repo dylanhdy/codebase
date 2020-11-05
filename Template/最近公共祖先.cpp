982010fc929fc1f7c091eedb1e515c1de9ffe63d
crazydave <40438517@qq.com>
2020-11-05 14:19:19 +0800

    update new files

--- Template/最近公共祖先.cpp
+++ Template/最近公共祖先.cpp
@@ -1,63 +1,70 @@
-#include <iostream>
 #include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
 #include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <vector>
+#define LL long long
+#define LD long double
 #define MAXN 500005
+#define MAXM
+#define P
+#define INF 0x3f3f3f3f
 using namespace std;
-int n,m,s,x,y,a,b,k,cnt,temp,num,fa[MAXN],deep[MAXN],l[MAXN],head[MAXN],lca[20][MAXN];
-bool vis[MAXN];
-struct Edge{
-    int next,to;
-}edge[MAXN*2];
 
-void addedge(int from,int to)
+int n, m, s, cnt, head[MAXN], dep[MAXN], f[MAXN][20];
+
+struct Edge {int to, nxt;} edge[MAXN*2];
+
+void addedge(int from, int to)
 {
-    edge[++num].next=head[from];
-    edge[num].to=to;
-    head[from]=num;
+    edge[++cnt].nxt=head[from];
+    edge[cnt].to=to;
+    head[from]=cnt;
 }
 
-void dfs(int node){ 
-    deep[node]=deep[fa[node]]+1;
-    for(int i=head[node]; i; i=edge[i].next)
-    	if (edge[i].to!=fa[node])
-		{
-			fa[edge[i].to]=node;
-        	dfs(edge[i].to);
-        }
-    return;
-}
-
-int query(int x, int y)
+void dfs(int x, int fa)
 {
-    if (deep[x] > deep[y]) swap(x,y);
-	for (int i=20; i>=0; i--)
-	 	if (deep[lca[i][y]]>=deep[x])
-	   		y=lca[i][y];
-	if (x==y) return x;
-	for (int i=20; i>=0; i--)
-		if (lca[i][x]!=lca[i][y])
-	   		x=lca[i][x],y=lca[i][y];
-	return fa[x];
-} 
-
-int main(){
-    scanf("%d%d%d",&n,&m,&s);
-    for (int i=1;i<n;i++){
-        scanf("%d%d",&x,&y);
-        addedge(x,y);
-        addedge(y,x);
+    f[x][0]=fa, dep[x]=dep[fa]+1;
+    for(int i=head[x]; i; i=edge[i].nxt)
+    {
+        int to=edge[i].to;
+        if(to==fa) continue;
+        dep[to]=dep[x]+1;
+        dfs(to, x);
     }
-    dfs(s);
-    for(int i=1; i<=n; i++) lca[0][i]=fa[i];
-    for(int i=1; i<=20; i++)
-        for(int j=1; j<=n; j++)
-        	lca[i][j]=lca[i-1][lca[i-1][j]];        
-    for (int i=1;i<=m;i++){
-        scanf("%d%d",&a,&b);
-        printf("%d\n",query(a, b));
+}
+
+int lca(int x, int y)
+{
+    if(dep[x]>dep[y]) swap(x, y);
+    for(int i=19; i>=0; --i)
+        if(dep[f[y][i]]>=dep[x]) y=f[y][i];
+    if(x==y) return x;
+    for(int i=19; i>=0; --i)
+        if(f[x][i]!=f[y][i]) x=f[x][i], y=f[y][i];
+    return f[x][0];
+}
+
+int main()
+{
+    scanf("%d%d%d", &n, &m);
+    for(int i=1; i<n; ++i)
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        addedge(x, y);
+        addedge(y, x);
+    }
+    dfs(1, 0);
+    for(int i=1; i<=19; ++i)
+        for(int j=1; j<=n; ++j) f[j][i]=f[f[j][i-1]][i-1];
+    for(int i=1; i<=m; ++i)
+    {
+        int x, y;
+        scanf("%d%d", &x, &y);
+        printf("%d\n", lca(x, y));
     }
     return 0;
 }
