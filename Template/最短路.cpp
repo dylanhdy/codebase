fe383b3df0f56cddc60229371dc1fe02faa80928
crazydave <40438517@qq.com>
2020-12-02 14:27:17 +0800

    update new files

---
+++ Template/最短路.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <vector>
+#define LL long long
+#define LD long double
+#define MAXN
+#define MAXM
+#define P
+#define mp make_pair
+#define pb push_back
+#define INF 0x3f3f3f3f
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
