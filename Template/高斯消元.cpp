7b7a9aff83f8c720bfbb8ad538043feb48677b18
crazydave <40438517@qq.com>
2021-04-30 22:00:15 +0800

    update new files

--- Template/高斯消元.cpp
+++ Template/高斯消元.cpp
@@ -1,52 +1,40 @@
-#include <cstdio>
-#include <algorithm>
-#include <cmath>
-#include <iostream>
-#include <cstring>
-#include <queue>
-#include <vector>
+#include <bits/stdc++.h>
 #define LL long long
 #define LD long double
 #define MAXN 105
 #define MAXM
 #define P
-#define eps 1e-4
+#define MP make_pair
+#define PB push_back
 #define INF 0x3f3f3f3f
+#define dbg(a...) fprintf(stderr, a)
 using namespace std;
 
 int n;
-double a[MAXN][MAXN], b[MAXN];
+double a[MAXN][MAXN];
 
 int main()
 {
     scanf("%d", &n);
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=1; j<=n; j++) scanf("%lf", &a[i][j]);
-        scanf("%lf", &b[i]);
+    for(int i=1; i<=n; ++i)
+        for(int j=1; j<=n+1; ++j) scanf("%lf", &a[i][j]);
+    for(int i=1; i<=n; ++i) {
+        int pos=0;
+        for(int j=i; j<=n; ++j)
+            if(a[j][i]!=0) pos=j;
+        if(pos==0) continue;
+        for(int j=i; j<=n+1; ++j) swap(a[i][j], a[pos][j]);
+        for(int j=n+1; j>=i; --j) a[i][j]/=a[i][i];
+        for(int j=1; j<=n; ++j) {
+            if(i==j) continue;
+            for(int k=n+1; k>=0; --k) a[j][k]-=a[j][i]*a[i][k];
+        }
     }
-    for(int i=1; i<=n; i++)
-    {
-        for(int j=i; j<=n; j++)
-            if(fabs(a[j][i])>eps)
-            {
-                for(int k=1; k<=n; k++) swap(a[j][k], a[i][k]); 
-                swap(b[j], b[i]);
-            }
-    	for(int j=1; j<=n; j++)
-            if(i!=j)
-            {
-                double rate=a[j][i]/a[i][i];
-                for(int k=i; k<=n; k++) a[j][k]-=a[i][k]*rate;
-                b[j]-=b[i]*rate;	
-            }
-    }
-    for(int i=1; i<=n; i++)
-        if(fabs(a[i][i])<eps || fabs(b[i]/a[i][i])<eps)
-        {
+    for(int i=1; i<=n; ++i)
+        if(a[i][i]==0) {
             printf("No Solution\n");
             return 0;
         }
-    for(int i=1; i<=n; i++) printf("%.2lf\n", b[i]/a[i][i]);
+    for(int i=1; i<=n; ++i) printf("%.2lf\n", a[i][n+1]);
     return 0;
 }
