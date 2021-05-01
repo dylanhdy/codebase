f21b1125f0f3ab895aa3164348c6e43088545f3d
crazydave <40438517@qq.com>
2021-05-01 11:37:31 +0800

    update new files

--- Template/高斯消元.cpp
+++ Template/高斯消元.cpp
@@ -18,23 +18,24 @@ int main()
     scanf("%d", &n);
     for(int i=1; i<=n; ++i)
         for(int j=1; j<=n+1; ++j) scanf("%lf", &a[i][j]);
-    for(int i=1; i<=n; ++i) {
+    int c=1, r=1;
+    for(; c<=n; ++c) {
         int pos=0;
-        for(int j=i; j<=n; ++j)
-            if(a[j][i]!=0) pos=j;
+        for(int j=r; j<=n; ++j)
+            if(a[j][c]!=0) pos=j;
         if(pos==0) continue;
-        for(int j=i; j<=n+1; ++j) swap(a[i][j], a[pos][j]);
-        for(int j=n+1; j>=i; --j) a[i][j]/=a[i][i];
+        for(int j=c; j<=n+1; ++j) swap(a[r][j], a[pos][j]);
+        for(int j=n+1; j>=c; --j) a[r][j]/=a[r][c];
         for(int j=1; j<=n; ++j) {
-            if(i==j) continue;
-            for(int k=n+1; k>=i; --k) a[j][k]-=a[j][i]*a[i][k];
+            if(r==j) continue;
+            for(int k=n+1; k>=c; --k) a[j][k]-=a[j][c]*a[r][k];
         }
+        r++;
     }
-    for(int i=1; i<=n; ++i)
-        if(a[i][i]==0) {
-            printf("No Solution\n");
-            return 0;
-        }
+    if(r<=n) {
+        printf("No Solution\n");
+        return 0;
+    }
     for(int i=1; i<=n; ++i) printf("%.2lf\n", a[i][n+1]);
     return 0;
 }
